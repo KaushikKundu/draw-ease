e87970ad1b94c0ba05c756cb6b6293d924fa3663
Kaushik kundu <kaushikkundu774@gmail.com>
2025-05-12 10:59:32 +0530

    initial folder structure

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+    ws.send(`Echo: ${message}`);
+  });
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
